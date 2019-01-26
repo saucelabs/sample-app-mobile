a9178dd7010a5c37a9e41e131db9020ef1766eb9
Wim Selles <wim@wswebcreation.nl>
2019-01-26 11:10:36 -0800

    feat: add Android splash screen incl module that will prevent flickering

--- src/js/App.js
+++ src/js/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import { View, StyleSheet } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 import NavigationContainer from './Router';
 import SwagLabsStatusBar from './components/StatusBar';
 
 export default class App extends Component {
+  componentDidMount() {
+    SplashScreen.hide();
+  }
+
   render() {
     return (
       <View style={ styles.container }>
