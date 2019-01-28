fef24dbca6667a348e892ce686fc5e870f4b95b5
Wim Selles <wim@wswebcreation.nl>
2019-01-27 19:57:53 -0800

    fix: fix last test

--- tests/e2e/spec/inventory.list.spec.js
+++ tests/e2e/spec/inventory.list.spec.js
@@ -20,8 +20,8 @@ describe('Inventory List Page', () => {
     expect(InventoryListScreen.swagItems.length).toBeGreaterThan(0, 'No items are shown');
   });
 
-  it('should be able to select a swag item and open the details page', () => {
-    InventoryListScreen.openSwagItemDetails('Bike Light');
+  fit('should be able to select a swag item and open the details page', () => {
+    InventoryListScreen.openSwagItemDetails('Sauce Labs Backpack');
 
     expect(InventoryItemScreen.waitForIsShown()).toEqual(true, 'The inventory item screen is not shown');
   });
