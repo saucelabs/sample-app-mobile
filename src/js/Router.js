fdc1711cdf24ccedfb1470688f45bcd7e08a7d68
Wim Selles <wim@wswebcreation.nl>
2019-08-13 11:22:58 +0200

    fix: fix for bubbling path from drawer to stack

--- src/js/Router.js
+++ src/js/Router.js
@@ -16,75 +16,78 @@ import WebviewScreen from './screens/Webview';
 import DrawerLinks from './components/DrawerLinks';
 
 export const SCREENS = {
-  LOGIN: 'Login',
-  INVENTORY_LIST: 'InventoryList',
-  INVENTORY_ITEM: 'InventoryItem',
-  CART_CONTENTS: 'CartContents',
-  CHECKOUT_SCREEN_ONE: 'CheckoutScreenOne',
-  CHECKOUT_SCREEN_TWO: 'CheckoutScreenTwo',
-  CHECKOUT_COMPLETE: 'CheckoutComplete',
-  WEBVIEW_SELECTION: 'WebviewSelection',
-  WEBVIEW_SCREEN: 'WebviewScreen',
+	LOGIN: 'Login',
+	INVENTORY_LIST: 'InventoryList',
+	INVENTORY_ITEM: 'InventoryItem',
+	CART_CONTENTS: 'CartContents',
+	CHECKOUT_SCREEN_ONE: 'CheckoutScreenOne',
+	CHECKOUT_SCREEN_TWO: 'CheckoutScreenTwo',
+	CHECKOUT_COMPLETE: 'CheckoutComplete',
+	WEBVIEW_SELECTION: 'WebviewSelection',
+	WEBVIEW_SCREEN: 'WebviewScreen',
 };
 
 const StackNavigator = createStackNavigator({
-    [SCREENS.LOGIN]: { screen: Login },
-    [SCREENS.INVENTORY_LIST]: {
-      screen: InventoryList ,
-      path: 'swag-overview',
-    },
-    [SCREENS.INVENTORY_ITEM]: {
-      screen: InventoryItem ,
-      path: 'swag-item/:item',
-    },
-    [SCREENS.CART_CONTENTS]: {
-      screen: CartContents ,
-      path: 'cart/:items',
-    },
-    [SCREENS.CHECKOUT_SCREEN_ONE]: {
-      screen: CheckoutScreenOne,
-      path: 'personal-info/',
-    },
-    [SCREENS.CHECKOUT_SCREEN_TWO]: {
-      screen: CheckoutScreenTwo,
-      path: 'checkout-overview/:items',
-    },
-    [SCREENS.CHECKOUT_COMPLETE]: {
-      screen: CheckoutComplete,
-      path: 'complete',
-    },
-    [SCREENS.WEBVIEW_SELECTION]: {
-      screen: WebviewSelection,
-      path: 'webview',
-    },
-    [SCREENS.WEBVIEW_SCREEN]: {
-      screen: WebviewScreen,
-      path: 'webview-selection/:url',
-    },
-  },
-  {
-    initialRouteName: SCREENS.LOGIN,
-    defaultNavigationOptions: ({ navigate, navigation }) => ({
-      header: (<AppHeader navigation={ navigation }/>),
-      gesturesEnabled: false,
-    }),
-  });
+		[ SCREENS.LOGIN ]: { screen: Login },
+		[ SCREENS.INVENTORY_LIST ]: {
+			screen: InventoryList,
+			path: 'swag-overview',
+		},
+		[ SCREENS.INVENTORY_ITEM ]: {
+			screen: InventoryItem,
+			path: 'swag-item/:item',
+		},
+		[ SCREENS.CART_CONTENTS ]: {
+			screen: CartContents,
+			path: 'cart/:items',
+		},
+		[ SCREENS.CHECKOUT_SCREEN_ONE ]: {
+			screen: CheckoutScreenOne,
+			path: 'personal-info/',
+		},
+		[ SCREENS.CHECKOUT_SCREEN_TWO ]: {
+			screen: CheckoutScreenTwo,
+			path: 'checkout-overview/:items',
+		},
+		[ SCREENS.CHECKOUT_COMPLETE ]: {
+			screen: CheckoutComplete,
+			path: 'complete',
+		},
+		[ SCREENS.WEBVIEW_SELECTION ]: {
+			screen: WebviewSelection,
+			path: 'webview',
+		},
+		[ SCREENS.WEBVIEW_SCREEN ]: {
+			screen: WebviewScreen,
+			path: 'webview-selection/:url',
+		},
+	},
+	{
+		initialRouteName: SCREENS.LOGIN,
+		defaultNavigationOptions: ({ navigate, navigation }) => ({
+			header: (<AppHeader navigation={ navigation }/>),
+			gesturesEnabled: false,
+		}),
+	});
 
 const DrawerNavigator = createDrawerNavigator({
-  StackNavigator: { screen: StackNavigator },
+	StackNavigator: {
+		screen: StackNavigator,
+		path: '',
+	},
 }, {
-  contentComponent: DrawerLinks,
-  drawerWidth: WINDOW_WIDTH,
-  gesturesEnabled: false,
+	contentComponent: DrawerLinks,
+	drawerWidth: WINDOW_WIDTH,
+	gesturesEnabled: false,
 });
 
 const Router = createAppContainer(DrawerNavigator);
 const prefix = 'swagLabs://';
 
 export default class NavigationContainer extends Component {
-  render() {
-    return (
-      <Router uriPrefix={ prefix }/>
-    );
-  }
+	render() {
+		return (
+			<Router uriPrefix={ prefix }/>
+		);
+	}
 }
