f7a5e6b1b4c44f0ee5dc71afc2b2825392fe0a25
Wim Selles <wim@wswebcreation.nl>
2019-09-02 21:44:29 +0200

    feat: Add Dutch locales

--- src/js/config/I18n.js
+++ src/js/config/I18n.js
@@ -1,6 +1,7 @@
 import I18n from 'i18n-js';
 import * as RNLocalize from 'react-native-localize';
 import en from './translations/en';
+import nl from './translations/nl';
 
 const locales = RNLocalize.getLocales();
 
@@ -11,6 +12,7 @@ if (Array.isArray(locales)) {
 I18n.fallbacks = true;
 I18n.translations = {
 	en,
+	nl,
 };
 
 export default I18n;
--- src/js/config/translations/en.js
+++ src/js/config/translations/en.js
@@ -148,4 +148,36 @@ export default {
 		'rights': '© 2019 Sauce Labs. All Rights Reserved.',
 		'termsAndPrivacy': 'Terms of Service | Privacy Policy',
 	},
+	'products': {
+		'backpack': {
+			name: 'Sauce Labs Backpack',
+			desc: 'carry.allTheThings() with the sleek, streamlined Sly Pack that melds uncompromising style with unequaled laptop and tablet protection.',
+			price: 29.99,
+		},
+		'bikeLight': {
+			name: 'Sauce Labs Bike Light',
+			desc: 'A red light isn\'t the desired state in testing but it sure helps when riding your bike at night. Water-resistant with 3 lighting modes, 1 AAA battery included.',
+			price: 9.99,
+		},
+		'boltShirt': {
+			name: 'Sauce Labs Bolt T-Shirt',
+			desc: 'Get your testing superhero on with the Sauce Labs bolt T-shirt. From American Apparel, 100% ringspun combed cotton, heather gray with red bolt.',
+			price: 15.99,
+		},
+		'fleeceJacket': {
+			name: 'Sauce Labs Fleece Jacket',
+			desc: 'It\'s not every day that you come across a midweight quarter-zip fleece jacket capable of handling everything from a relaxing day outdoors to a busy day at the office.',
+			price: 49.99,
+		},
+		'onesie': {
+			name: 'Sauce Labs Onesie',
+			desc: 'Rib snap infant onesie for the junior automation engineer in development. Reinforced 3-snap bottom closure, two-needle hemmed sleeved and bottom won\'t unravel.',
+			price: 7.99,
+		},
+		'tattRed': {
+			name: 'Test.allTheThings() T-Shirt (Red)',
+			desc: 'This classic Sauce Labs t-shirt is perfect to wear when cozying up to your keyboard to automate a few tests. Super-soft and comfy ringspun combed cotton.',
+			price: 15.99,
+		},
+	},
 };
