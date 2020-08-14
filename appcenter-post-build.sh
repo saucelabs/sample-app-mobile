ed377a38c60b248d9b727d2224c6f4f5c465816b
Wim Selles <wim@wswebcreation.nl>
2020-08-14 16:42:26 +0200

    chore: fix script

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+# In order for this script to work you need to:
+# - AppCenter
+# - Your project
+# - Config your project
+# - Then you should see the post-script, then SAVE it, otherwise it will not work
+# See src: https://github.com/MicrosoftDocs/appcenter-docs/issues/53
+
 
 APP_NAME=""
 # You can find this name in the build logs
@@ -15,16 +22,18 @@ fi
 
 echo "**************** PUBLISH APP TO SAUCELABS WITH THIS DATA ******************"
 echo "APP NAME                => $APP_NAME"
+echo "BUILD NAME              => $BUILD_NAME"
 echo "OUTPUT DIRECTORY        => $APPCENTER_OUTPUT_DIRECTORY"
+echo "PAYLOAD                 => $APPCENTER_OUTPUT_DIRECTORY/$BUILD_NAME"
 
 # Push to US
 curl \
-  -F "payload=@$APPCENTER_OUTPUT_DIRECTORY/$BUILD_NAME" \
+  -F "apk=@$APPCENTER_OUTPUT_DIRECTORY/$BUILD_NAME" \
   -F name=$APP_NAME \
   -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY"  'https://api.us-west-1.saucelabs.com/v1/storage/upload'
 
 # Push to EU
 curl \
-  -F "payload=@$APPCENTER_OUTPUT_DIRECTORY/$BUILD_NAME" \
+  -F "ipa=@$APPCENTER_OUTPUT_DIRECTORY/$BUILD_NAME" \
   -F name=$APP_NAME \
   -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY"  'https://api.eu-central-1.saucelabs.com/v1/storage/upload'
