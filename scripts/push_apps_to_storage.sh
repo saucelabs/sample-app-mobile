4f091f5855f70ab1c0e0e0f441ef19e4735e308a
Wim Selles <wim@wswebcreation.nl>
2020-10-01 09:13:40 +0200

    chore: update upload script

--- scripts/push_apps_to_storage.sh
+++ scripts/push_apps_to_storage.sh
@@ -3,27 +3,37 @@
 ## US
 # Android
 curl \
-  -F 'payload=@../apps/Android.SauceLabs.Mobile.Sample.app.2.3.0.apk' \
+  -F 'payload=@../apps/Android.SauceLabs.Mobile.Sample.app.2.4.0.apk' \
   -F name=sample-app-android.apk \
   -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY"  \
   'https://api.us-west-1.saucelabs.com/v1/storage/upload'
 # iOS
 curl \
-  -F 'payload=@../apps/iOS.Simulator.SauceLabs.Mobile.Sample.app.2.3.0.zip' \
+  -F 'payload=@../apps/iOS.Simulator.SauceLabs.Mobile.Sample.app.2.4.0.zip' \
   -F name=sample-app-ios.zip \
+  -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY"  \
+  'https://api.us-west-1.saucelabs.com/v1/storage/upload'
+curl \
+  -F 'payload=@../apps/iOS.Simulator.SauceLabs.Mobile.Sample.app.2.4.0.ipa' \
+  -F name=sample-app-ios.ipa \
   -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY"  \
   'https://api.us-west-1.saucelabs.com/v1/storage/upload'
 
 ## EU
 # Android
 curl \
-  -F 'payload=@../apps/Android.SauceLabs.Mobile.Sample.app.2.3.0.apk' \
+  -F 'payload=@../apps/Android.SauceLabs.Mobile.Sample.app.2.4.0.apk' \
   -F name=sample-app-android.apk \
   -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY"  \
   'https://api.eu-central-1.saucelabs.com/v1/storage/upload'
 # iOS
 curl \
-  -F 'payload=@../apps/iOS.Simulator.SauceLabs.Mobile.Sample.app.2.3.0.zip' \
+  -F 'payload=@../apps/iOS.Simulator.SauceLabs.Mobile.Sample.app.2.4.0.zip' \
   -F name=sample-app-ios.zip \
+  -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY"  \
+  'https://api.eu-central-1.saucelabs.com/v1/storage/upload'
+curl \
+  -F 'payload=@../apps/iOS.Simulator.SauceLabs.Mobile.Sample.app.2.4.0.ipa' \
+  -F name=sample-app-ios.ipa \
   -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY"  \
   'https://api.eu-central-1.saucelabs.com/v1/storage/upload'
