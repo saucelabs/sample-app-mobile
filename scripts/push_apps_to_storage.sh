5f9295e9d1ae30a0893c0351a3107377d4c2c03e
Wim Selles <wim@wswebcreation.nl>
2020-10-07 18:45:49 +0200

    chore: auto determine version for push script

--- scripts/push_apps_to_storage.sh
+++ scripts/push_apps_to_storage.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
+VERSION=$(cat ../package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
+
 ## US
 # Android
 curl \
-  -F 'payload=@../apps/Android.SauceLabs.Mobile.Sample.app.2.4.0.apk' \
+  -F "payload=@../apps/Android.SauceLabs.Mobile.Sample.app.$VERSION.apk" \
   -F name=sample-app-android.apk \
   -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY"  \
   'https://api.us-west-1.saucelabs.com/v1/storage/upload'
 # iOS
 curl \
-  -F 'payload=@../apps/iOS.Simulator.SauceLabs.Mobile.Sample.app.2.4.0.zip' \
+  -F "payload=@../apps/iOS.Simulator.SauceLabs.Mobile.Sample.app.$VERSION.zip" \
   -F name=sample-app-ios.zip \
   -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY"  \
   'https://api.us-west-1.saucelabs.com/v1/storage/upload'
 curl \
-  -F 'payload=@../apps/iOS.Simulator.SauceLabs.Mobile.Sample.app.2.4.0.ipa' \
+  -F "payload=@../apps/iOS.Simulator.SauceLabs.Mobile.Sample.app.$VERSION.ipa" \
   -F name=sample-app-ios.ipa \
   -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY"  \
   'https://api.us-west-1.saucelabs.com/v1/storage/upload'
@@ -22,18 +29,18 @@ curl \
 ## EU
 # Android
 curl \
-  -F 'payload=@../apps/Android.SauceLabs.Mobile.Sample.app.2.4.0.apk' \
+  -F "payload=@../apps/Android.SauceLabs.Mobile.Sample.app.$VERSION.apk" \
   -F name=sample-app-android.apk \
   -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY"  \
   'https://api.eu-central-1.saucelabs.com/v1/storage/upload'
 # iOS
 curl \
-  -F 'payload=@../apps/iOS.Simulator.SauceLabs.Mobile.Sample.app.2.4.0.zip' \
+  -F "payload=@../apps/iOS.Simulator.SauceLabs.Mobile.Sample.app.$VERSION.zip" \
   -F name=sample-app-ios.zip \
   -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY"  \
   'https://api.eu-central-1.saucelabs.com/v1/storage/upload'
 curl \
-  -F 'payload=@../apps/iOS.Simulator.SauceLabs.Mobile.Sample.app.2.4.0.ipa' \
+  -F "payload=@../apps/iOS.Simulator.SauceLabs.Mobile.Sample.app.$VERSION.ipa" \
   -F name=sample-app-ios.ipa \
   -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY"  \
   'https://api.eu-central-1.saucelabs.com/v1/storage/upload'
